2d5f2e1b4150f9988c40e8ea568aae7db691edce
standers <standers@nerdery.com>
2015-02-07 13:40:00 -0600

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name troutDashApp
+ * @description
+ * # troutDashApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('troutDashApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name troutDashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the troutDashApp
+ */
+angular.module('troutDashApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
