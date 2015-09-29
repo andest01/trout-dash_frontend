a36fdb8ae466c55963fda1b72d09915e76cdd307
Stuart Anderson <andest01@gmail.com>
2015-09-29 12:24:41 -0500

    yeah!

--- Gruntfile.js
+++ Gruntfile.js
@@ -326,7 +326,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
-            'app/data/**/*',
+            'data/**/*',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
